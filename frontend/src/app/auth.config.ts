cb86bca3a440b167ce5b97c72a47fc537a9a13cf
Andres Villegas <thisisvillegas@gmail.com>
2025-12-07 17:08:12 -0600

    migrating to platform

--- frontend/src/app/auth.config.ts
+++ frontend/src/app/auth.config.ts
@@ -2,10 +2,10 @@ import { AuthConfig } from '@auth0/auth0-angular';
 
 export const authConfig: AuthConfig = {
     domain: 'dev-6sfjxompkc4vz884.us.auth0.com',  // e.g., 'dev-xxxxx.us.auth0.com'
-    clientId: 'hbh4yFBax7RFG5jyeQBdiFAJucSSwJMH',
+    clientId: '3cUknJuXEju8xlwS9RIP4t1SAxrdJfw4',
     authorizationParams: {
         redirect_uri: window.location.origin + '/callback',
-        audience: 'https://api.racing-dashboard.com',
+        audience: 'https://api-home.thisisvillegas.com',
     },
     httpInterceptor: {
         allowedList: [
@@ -13,15 +13,15 @@ export const authConfig: AuthConfig = {
                 uri: 'http://localhost:3000/api/*',
                 tokenOptions: {
                     authorizationParams: {
-                        audience: 'https://api.racing-dashboard.com',
+                        audience: 'https://api-home.thisisvillegas.com',
                     }
                 }
             },
             {
-                uri: 'https://api.thisisvillegas.com/api/*',
+                uri: 'https://api-home.thisisvillegas.com/api/*',
                 tokenOptions: {
                     authorizationParams: {
-                        audience: 'https://api.racing-dashboard.com',
+                        audience: 'https://api-home.thisisvillegas.com',
                     }
                 }
             }
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.thisisvillegas.com/api'
+    apiUrl: 'https://api-home.thisisvillegas.com/api'
 };
